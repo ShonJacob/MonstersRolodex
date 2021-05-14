42bc8593aff1bb2e31648f017a74b795a3750425
Shon Jacob <shonmatt@hotmail.com>
2021-05-14 15:47:18 +0530

    Updates

---
+++ precache-manifest.3ab044fce3767f8251f744effb58d955.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a3ce84524e714bdf677847b46f0150",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "7bf1f13ab4de2fb07ef4",
+    "url": "/MonstersRolodex/static/css/main.7bae9674.chunk.css"
+  },
+  {
+    "revision": "9aa7d682bd3a5808e6a3",
+    "url": "/MonstersRolodex/static/js/2.ca414bc8.chunk.js"
+  },
+  {
+    "revision": "23550d90aacdf7326453",
+    "url": "/MonstersRolodex/static/js/3.5078a456.chunk.js"
+  },
+  {
+    "revision": "7bf1f13ab4de2fb07ef4",
+    "url": "/MonstersRolodex/static/js/main.542fb638.chunk.js"
+  },
+  {
+    "revision": "ecab2871495196382742",
+    "url": "/MonstersRolodex/static/js/runtime~main.6617f357.js"
+  }
+]);
